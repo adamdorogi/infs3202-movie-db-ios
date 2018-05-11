e06d9676c3bf208fef58ce99939fc7f12794e919
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2018-05-12 07:42:06 +1000

    Initial Commit

---
+++ INFS3202Tests/INFS3202Tests.swift
@@ -0,0 +1,36 @@
+//
+//  INFS3202Tests.swift
+//  INFS3202Tests
+//
+//  Created by Adam Dorogi-Kaposi on 12/5/18.
+//  Copyright © 2018 Adam Dorogi-Kaposi. All rights reserved.
+//
+
+import XCTest
+@testable import INFS3202
+
+class INFS3202Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
